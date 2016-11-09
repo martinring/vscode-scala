36315e593e2261bcde8e821eb71753411ffb30a0
Martin Ring <martin.ring@dfki.de>
2016-11-09 08:18:00 +0100

    wip

--- server/src/main/scala/net/flatmap/vscode/ScalaServer.scala
+++ server/src/main/scala/net/flatmap/vscode/ScalaServer.scala
@@ -1,64 +1,106 @@
 package net.flatmap.vscode
 
 import java.io.File
+import java.net.URI
 
-import akka.actor.ActorSystem
+import akka.pattern.ask
+import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.{Materializer, OverflowStrategy}
 import akka.stream.scaladsl.{Sink, Source}
+import akka.util.Timeout
 import com.google.common.base.Charsets
 import com.google.common.io.Files
 import io.circe.Json
 import net.flatmap.jsonrpc.{ErrorCodes, ResponseError}
+import net.flatmap.vscode.languageserver.ServerCapabilities.CompletionProvider
 import net.flatmap.vscode.languageserver._
 import org.ensime.api.{TextEdit => _, _}
 import org.ensime.config.EnsimeConfigProtocol
 import org.ensime.core.{Broadcaster, Project}
 
+import scala.collection.mutable
 import scala.concurrent._
+import scala.concurrent.duration._
 import scala.util.Try
 
 class ScalaServer(client: LanguageClient)(implicit ec: ExecutionContext, mat: Materializer, system: ActorSystem) extends
   LanguageServer
-  with ServerCapabilities.CompletionProvider
-  with TextDocuments
+  with CompletionProvider
   with Configuration[Config] {
 
   VSCodeLogger.instance.trySuccess(client)
 
-  var maxNumberOfProblems = 100
+  override def textDocumentSyncKind: TextDocumentSyncKind = TextDocumentSyncKind.None
+
+  val project = Promise[ActorRef]
+  val self = Promise[ActorRef]
+
+  val notes = mutable.Buffer.empty[Note]
+
+  val messageHandler = Sink.foreach[EnsimeServerMessage] {
+    case ClearAllScalaNotesEvent =>
+      notes.clear()
+    case NewScalaNotesEvent(full, newNotes) =>
+      notes.appendAll(newNotes)
+    case FullTypeCheckCompleteEvent =>
+      publishDiagnostics()
+    case msg =>
+      client.window.logMessage(MessageType.Info, "RECEIVED UNHANDLED: " + msg.toString)
+  }
+
+  def ensime: ActorRef = project.future.value.map(_.get).getOrElse(system.deadLetters)
+
+  def publishDiagnostics() = {
+    notes.foldLeft(Map.empty[URI, Seq[Diagnostic]]) {
+      case (notes, note) =>
+        val uri = new File(note.file).toURI
+        docs.get(uri).fold {
+          notes
+        } { doc =>
+          val old = notes.getOrElse(uri, Seq.empty)
+          val diag: Diagnostic = Diagnostic(
+            Range(doc.positionAt(note.beg),doc.positionAt(note.end)),
+            note.msg,
+            Some(note.severity match {
+              case NoteError => DiagnosticSeverity.Error
+              case NoteWarn  => DiagnosticSeverity.Warning
+              case NoteInfo  => DiagnosticSeverity.Information
+            })
+          )
+          notes.updated(uri, old :+ diag)
+        }
+    }.foreach { case (uri, diags) =>
+      client.textDocument.publishDiagnostics(uri, diags)
+    }
+  }
+
+  val docs = mutable.Map.empty[URI, TextDocumentItem]
+
+  def sourceFileInfo(doc: TextDocument) = SourceFileInfo(new File(doc.uri),Some(doc.text))
 
   def initialize(processId: Option[Int],
                  rootPath: Option[String],
                  initializationOptions: Option[Json],
                  capabilities: ClientCapabilities,
                  trace: Option[Trace]): Future[InitializeResult] = {
+    client.window.logMessage(MessageType.Log, "starting up scala server")
     new File(rootPath.getOrElse(".") + "/.ensime_cache").mkdir()
     val res = Try {
       EnsimeConfigProtocol.parse(Files.toString(new File(rootPath.getOrElse(".") + "/.ensime"), Charsets.UTF_8))
     }.map { implicit config =>
       val broadcaster = system.actorOf(Broadcaster(), "broadcaster")
-      broadcaster ! Broadcaster.Register
-      val msgs = Source.actorRef[EnsimeServerMessage](1024,OverflowStrategy.fail)
-      val handler = Sink.foreach[EnsimeServerMessage] {
-        case FullTypeCheckCompleteEvent =>
-        case msg => client.window.logMessage(MessageType.Info, msg.toString)
-      }
-      val ref = msgs.to(handler).run()
-      broadcaster.tell(Broadcaster.Register,ref)
+      val msgs = Source.actorRef[EnsimeServerMessage](1024, OverflowStrategy.fail)
+      val ref = msgs.to(messageHandler).run()
+      this.self.trySuccess(ref)
+      broadcaster.tell(Broadcaster.Register, ref)
       val project = system.actorOf(Project(broadcaster), "project")
-      Thread.sleep(1000)
-      project.tell(ConnectionInfoReq,ref)
-      textDocuments.runForeach {
-        case (uri,src) =>
-          src.runForeach { doc =>
-            project ! TypecheckFileReq(SourceFileInfo(new File(uri), Some(doc.text)))
-          }
-      }
+      this.project.trySuccess(project)
+      project.tell(ConnectionInfoReq, ref)
       client.window.logMessage(MessageType.Log, "initialized ensime")
     }
     res.recover {
       case t =>
-        client.window.showMessage(MessageType.Error,t.getMessage)
+        client.window.showMessage(MessageType.Error, t.getMessage)
         ResponseError(
           ErrorCodes.serverErrorStart,
           t.getMessage,
@@ -74,47 +116,54 @@ class ScalaServer(client: LanguageClient)(implicit ec: ExecutionContext, mat: Ma
   def exit(): Unit =
     sys.exit()
 
-  def validateTextDocument(textDocument: TextDocumentItem): Unit = {
-    val diags = textDocument.text.lines.toSeq.zipWithIndex.flatMap { case (line,n) =>
-      val index = line.indexOf("typescript")
-      if (index >= 0) Seq[Diagnostic](
-        Diagnostic(
-          range = Range(n,index,n,index + 10),
-          severity = Some(DiagnosticSeverity.Warning),
-          code = None,
-          message = s"${line.substring(index,10)} should be spelled TypeScript",
-          source = Some("ex")
-        )
-      )
-      else Seq.empty[Diagnostic]
-    }.take(maxNumberOfProblems)
-    client.textDocument.publishDiagnostics(textDocument.uri, diags)
-  }
-
   def didChangeWatchedFiles(changes: Seq[FileEvent]): Unit = {
     // Monitored files have changed in VSCode
     client.window.logMessage(MessageType.Log, "We received a file change event")
   }
 
+  def didOpenDocument(textDocument: TextDocumentItem): Unit = {
+    docs += textDocument.uri -> textDocument
+    ensime ? TypecheckFileReq(SourceFileInfo(new File(textDocument.uri),Some(textDocument.text)))
+  }
+
+  def didChangeDocument(textDocument: VersionedTextDocumentIdentifier,
+                                 contentChanges: Seq[TextDocumentContentChangeEvent]): Unit = {
+    for {
+      state <- docs.get(textDocument.uri)
+      last <- contentChanges.lastOption
+    } {
+      docs(state.uri) = state.copy(
+        text = last.text,
+        version = textDocument.version
+      )
+      ensime ? TypecheckFileReq(SourceFileInfo(new File(textDocument.uri),Some(last.text)))
+    }
+  }
+
+  def didCloseDocument(textDocument: TextDocumentIdentifier): Unit = {
+    docs.remove(textDocument.uri)
+    ensime ? RemoveFileReq(new File(textDocument.uri))
+  }
+
+  def didSaveDocument(textDocument: TextDocumentIdentifier): Unit = {
+    // TODO
+  }
+
   override def completionOptions: CompletionOptions =
     CompletionOptions(resolveProvider = true)
 
-  @net.flatmap.jsonrpc.JsonRPC.Named("textDocument/completion")
+  implicit val defaultTimeout = Timeout(5 seconds)
   override def completion(textDocument: TextDocumentIdentifier,
-                 position: Position): Future[CompletionList] = Future.successful {
-    CompletionList(Seq(
-      CompletionItem(
-        label = "TypeScript",
-        kind = Some(CompletionItemKind.Text),
-        data = Some(Json.fromInt(1))
-      ),
-      CompletionItem(
-        label = "JavaScript",
-        kind = Some(CompletionItemKind.Text),
-        data = Some(Json.fromInt(2))
-      )
-    ))
-  }
+                          position: Position): Future[CompletionList] = (for {
+    doc <- docs.get(textDocument.uri)
+  } yield for {
+    response <- (ensime ? CompletionsReq(sourceFileInfo(doc), doc.offsetAt(position), 100, false, false)).mapTo[CompletionInfoList]
+  } yield {
+    val completionItems = response.completions.map { item =>
+      CompletionItem(item.name)
+    }
+    CompletionList(completionItems,isIncomplete = true)
+  }).getOrElse(Future.failed(ResponseError(ErrorCodes.InvalidRequest, "document is not known")))
 
   override def resolveCompletionItem(item: CompletionItem): Future[CompletionItem] = Future.successful {
     if (item.data == Some(Json.fromInt(1)))
@@ -128,12 +177,9 @@ class ScalaServer(client: LanguageClient)(implicit ec: ExecutionContext, mat: Ma
     else item
   }
 
-  @net.flatmap.jsonrpc.JsonRPC.Named("$/setTraceNotification")
   def setTrace(trace: Trace): Unit = ()
 
-  @net.flatmap.jsonrpc.JsonRPC.Named("textDocument/willSave")
   def willSaveDocument(textDocument: TextDocumentIdentifier, reason: TextDocumentSaveReason): Unit = ()
 
-  @net.flatmap.jsonrpc.JsonRPC.Named("textDocument/willSaveWaitUntil")
   def willSaveDocumentWaitUntil(textDocument: TextDocumentIdentifier, reason: TextDocumentSaveReason): Future[Seq[TextEdit]] = Future.successful(Seq.empty)
 }
--- server/src/main/scala/net/flatmap/vscode/Server.scala
+++ server/src/main/scala/net/flatmap/vscode/Server.scala
@@ -1,16 +1,46 @@
 package net.flatmap.vscode
 
-import java.io.{FileOutputStream, PrintStream}
+import java.io.{OutputStream, PrintStream}
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+import io.circe.{Encoder, Json}
 import net.flatmap.jsonrpc._
 import net.flatmap.vscode.languageserver._
 
-object Server extends LanguageServerApp {
-	override type Capabilities = LanguageServer
+object Server extends App {
+	val stdout = System.out
+	// We want to redirect all printlns etc away from stdout
+	System.setOut(new PrintStream(new OutputStream {
+		def write(b: Int): Unit = ()
+	}))
+
+	implicit val system = ActorSystem("vscode-scala", com.typesafe.config.ConfigFactory.parseResources("application.conf"))
+	implicit val materializer = ActorMaterializer()
+	implicit val dispatcher = system.dispatcher
+
+	type Capabilities = LanguageServer
 		with ServerCapabilities.CompletionProvider
+
+	def openConnection(constructor: (LanguageClient with RemoteConnection) => Capabilities):
+	Connection[Capabilities,LanguageClient with RemoteConnection] = {
+		import net.flatmap.vscode.languageserver.Codec._
+
+		implicit val encodeDiagnostics = Encoder.instance[Seq[Diagnostic]] { diags =>
+			Json.arr(diags.map(encodeDiagnostic.apply) :_*)
+		}
+
+		val client = Remote[LanguageClient](Id.standard)
+		val server = Local[Capabilities]
+
+		val connection = Connection.bidi(server, client, constructor)
+
+		val in = StreamConverters.fromInputStream(() => System.in)
+		val out = StreamConverters.fromOutputStream(() => stdout)
+
+		in.viaMat(connection)(Keep.right).to(out).run()
+	}
 
 	val connection = openConnection(client => new ScalaServer(client))
 }
